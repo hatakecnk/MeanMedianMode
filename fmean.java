9ecd49dff53279554d543efdacb45211e79b102f
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2021-10-01 10:56:24 +0700

    Update fmean.java
    Fix BUG

--- fmean.java
+++ fmean.java
@@ -1,23 +1,24 @@
-import java.util.*;
+// Author : Febry Afriansyah //
+// Github : https://github.com/hatakecnk //
+
+import java.util.*; // Import semua module yang ada di java.util //
 public class tes{
     public static void main(String[] args){
         int input, abc;
         Scanner febry = new Scanner(System.in);
         for(String m = "Y"; m.equals("Y")||m.equals("y"); ){
-            try{
+            try{ // Menampilkan menu pilihan //
                 System.out.print("\nSimple Program Menghitung Mean, Median dan Modus\nAuthor\t: Febry Afriansyah\nGithub\t: https://github.com/hatakecnk\n\n1. Menghitung mean\n2. Menghitung median\n3. Menghitung modus\n0. Keluar\n");
                 System.out.print("Masukan Pilihan Anda (1/2/3/0): ");
                 abc = febry.nextInt();
-                if (abc==1){
-                    System.out.println("");
-                    System.out.print("Masukkan berapa banyak data: ");
+                if (abc==1){ // pilihan 1 menghitung mean //
+                    System.out.print("\nMasukkan berapa banyak data: ");
                     Scanner feb = new Scanner(System.in);
                     input = feb.nextInt();
                     if (input > 0){
                         System.out.println("\nMasukkan isi data berupa angka:");
                     int[] data =  new int[input];
-                    for (int i = 0; i < input; i++)
-                    {
+                    for (int i = 0; i < input; i++){
                         System.out.print("- Isi Data ke-" +(i + 1)+ " = ");
                         data[i] = feb.nextInt();
                     }
@@ -30,16 +31,14 @@ public class tes{
                     else
                         System.err.println("Silahkan masukkan banyak data lebih dari 0");
                 }
-                else if (abc==2){
-                    System.out.println("");
-                    System.out.print("Masukkan berapa banyak data: ");
+                else if (abc==2){ // Pilihan 2 menghitung median //
+                    System.out.print("\nMasukkan berapa banyak data: ");
                     Scanner feb = new Scanner(System.in);
                     input = feb.nextInt();
                     if (input > 0){
                         System.out.println("\nMasukkan isi data berupa angka:");
                     int[] data =  new int[input];
-                    for (int i = 0; i < input; i++)
-                    {
+                    for (int i = 0; i < input; i++){
                         System.out.print("- Isi Data ke-" +(i + 1)+ " = ");
                         data[i] = feb.nextInt();
                     }
@@ -52,16 +51,14 @@ public class tes{
                     else
                         System.err.println("Silahkan masukkan banyak data lebih dari 0");
                 }
-                else if (abc==3){
-                    System.out.println("");
-                    System.out.print("Masukkan berapa banyak data: ");
+                else if (abc==3){ // pilihan 3 menghitung modus //
+                    System.out.print("\nMasukkan berapa banyak data: ");
                     Scanner feb = new Scanner(System.in);
                     input = feb.nextInt();
                     if (input > 0){
                         System.out.println("\nMasukkan isi data berupa angka:");
                     int[] data =  new int[input];
-                    for (int i = 0; i < input; i++)
-                    {
+                    for (int i = 0; i < input; i++){
                         System.out.print("- Isi Data ke-" +(i + 1)+ " = ");
                         data[i] = feb.nextInt();
                     }
@@ -74,7 +71,7 @@ public class tes{
                     else
                         System.err.println("Silahkan masukkan banyak data lebih dari 0");
                 }
-                else{
+                else{ // Keluar //
                     System.out.println("Terima kasih telah menggunakan program kami");
                     System.exit(1);
                 }
@@ -91,56 +88,40 @@ public class tes{
         }
     }
 
-    static String modus(int[] data, int input)
-    {
+    static String modus(int[] data, int input){ // hitung modus //
         int c;
         int[] b = new int[input];
-
-        for (int i = 0; i < input; i++)
-        {
+        for (int i = 0; i < input; i++){
             c = 1;
-
             if (data[i] == -1)
                 b[i] = 0;
-            else
-            {
-                for (int j = i+1; j < input; j++)
-                {
-                    if (data[i] == data[j])
-                    {
+            else{
+                for (int j = i+1; j < input; j++){
+                    if (data[i] == data[j]){
                         c++;
                         data[j] = -1;
                     }
                 }
-
                 b[i] = c;
             }
         }
-
         int m = b[0];
-        for (int i = 1; i < input; i++)
-        {
+        for (int i = 1; i < input; i++){
             if (b[i] >= m)
                 m = b[i];
         }
-
         String[] outputHasil = new String[input];
-        for (int i = 0; i < input; i++)
-        {
+        for (int i = 0; i < input; i++){
             if (b[i] == m)
                 outputHasil[i] = String.valueOf(data[i]);
         }
-
         String[] modus = Arrays.stream(outputHasil).filter(Objects::nonNull).toArray(String[]::new);
-
         return String.join(", ", modus);
     }
 
-    static double median(int[] dataArr)
-    {
+    static double median(int[] dataArr){ // Hitung median //
         double median;
         int[] data = urutkan(dataArr);
-
         if (data.length % 2 == 1)
             median = data[data.length / 2];
         else
@@ -149,33 +130,25 @@ public class tes{
         return median;
     }
 
-    static double mean(int[] data)
-    {
+    static double mean(int[] data){ // Hitung mean //
         int tambah = 0;
-        for (int dataTambah : data)
-        {
+        for (int dataTambah : data){
             tambah += dataTambah;
         }
-
         return tambah / (double) data.length;
     }
 
-    static int[] urutkan(int[] data)
-    {
+    static int[] urutkan(int[] data){
         int temp;
-        for (int i = 0; i < data.length-1; i++)
-        {
-            for (int j = data.length-1; j > i; j--)
-            {
-                if (data[j-1] > data[j])
-                {
+        for (int i = 0; i < data.length-1; i++){
+            for (int j = data.length-1; j > i; j--){
+                if (data[j-1] > data[j]){
                     temp        = data[j];
                     data[j]     = data[j-1];
                     data[j-1]   = temp;
                 }
             }
         }
-
         return data;
     }
 }
