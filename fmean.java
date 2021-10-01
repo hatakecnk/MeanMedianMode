48647b1f43f4909cfde5639aedc6a347e4efce83
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2021-10-01 10:45:25 +0700

    fix bug

--- fmean.java
+++ fmean.java
@@ -1,107 +1,95 @@
 import java.util.*;
-public class fmean{
+public class tes{
     public static void main(String[] args){
         int input, abc;
         Scanner febry = new Scanner(System.in);
-        try{
-            for(String m = "Y"; m.equals("Y")||m.equals("y"); ){
-                System.out.print("Simple Program Menghitung Mean, Median dan Modus\nAuthor\t: Febry Afriansyah\nGithub\t: https://github.com/hatakecnk\n\n1. Menghitung mean\n2. Menghitung median\n3. Menghitung modus\n0. Keluar\n");
+        for(String m = "Y"; m.equals("Y")||m.equals("y"); ){
+            try{
+                System.out.print("\nSimple Program Menghitung Mean, Median dan Modus\nAuthor\t: Febry Afriansyah\nGithub\t: https://github.com/hatakecnk\n\n1. Menghitung mean\n2. Menghitung median\n3. Menghitung modus\n0. Keluar\n");
                 System.out.print("Masukan Pilihan Anda (1/2/3/0): ");
                 abc = febry.nextInt();
-                try{
-                    if (abc==1){
-                        System.out.println("");
-                        System.out.print("Masukkan berapa banyak data: ");
-                        Scanner feb = new Scanner(System.in);
-                        input = feb.nextInt();
-                        if (input > 0){
-                            System.out.println("\nMasukkan isi data berupa angka:");
-                        int[] data =  new int[input];
-                        for (int i = 0; i < input; i++)
-                        {
-                            System.out.print("- Isi Data ke-" +(i + 1)+ " = ");
-                            data[i] = feb.nextInt();
-                        }
-                        System.out.println("\nHasil :");
-                        System.out.printf("Mean\t= %.1f%n", mean(data));
-                        System.out.print("Apakah anda ingin kembali ke menu utama? y/t: ");
-                        Scanner lagi = new Scanner(System.in);
-                        m = lagi.next();
-                        }
-                        else
-                            System.err.println("Silahkan masukkan banyak data lebih dari 0");
+                if (abc==1){
+                    System.out.println("");
+                    System.out.print("Masukkan berapa banyak data: ");
+                    Scanner feb = new Scanner(System.in);
+                    input = feb.nextInt();
+                    if (input > 0){
+                        System.out.println("\nMasukkan isi data berupa angka:");
+                    int[] data =  new int[input];
+                    for (int i = 0; i < input; i++)
+                    {
+                        System.out.print("- Isi Data ke-" +(i + 1)+ " = ");
+                        data[i] = feb.nextInt();
                     }
-                    else if (abc==2){
-                        System.out.println("");
-                        System.out.print("Masukkan berapa banyak data: ");
-                        Scanner feb = new Scanner(System.in);
-                        input = feb.nextInt();
-                        if (input > 0){
-                            System.out.println("\nMasukkan isi data berupa angka:");
-                        int[] data =  new int[input];
-                        for (int i = 0; i < input; i++)
-                        {
-                            System.out.print("- Isi Data ke-" +(i + 1)+ " = ");
-                            data[i] = feb.nextInt();
-                        }
-                        System.out.println("\nHasil :");
-                        System.out.printf("Median\t= %.1f%n", median(data));
-                        System.out.print("Apakah anda ingin kembali ke menu utama? y/t: ");
-                        Scanner lagi = new Scanner(System.in);
-                        m = lagi.next();
-                        }
-                        else
-                            System.err.println("Silahkan masukkan banyak data lebih dari 0");
-                    }
-                    else if (abc==3){
-                        System.out.println("");
-                        System.out.print("Masukkan berapa banyak data: ");
-                        Scanner feb = new Scanner(System.in);
-                        input = feb.nextInt();
-                        if (input > 0){
-                            System.out.println("\nMasukkan isi data berupa angka:");
-                        int[] data =  new int[input];
-                        for (int i = 0; i < input; i++)
-                        {
-                            System.out.print("- Isi Data ke-" +(i + 1)+ " = ");
-                            data[i] = feb.nextInt();
-                        }
-                        System.out.println("\nHasil :");
-                        System.out.printf("Modus\t= %s%n", modus(data, input));
-                        System.out.print("Apakah anda ingin kembali ke menu utama? y/t: ");
-                        Scanner lagi = new Scanner(System.in);
-                        m = lagi.next();
-                        }
-                        else
-                            System.err.println("Silahkan masukkan banyak data lebih dari 0");
-                    }
-                    else{
-                        System.out.println("Terima kasih telah menggunakan program kami");
-                        System.exit(1);
-                    }
-                }
-                catch (InputMismatchException e) {
-                    System.out.print("Input Salah!");
-                    System.out.print("\nApakah anda ingin kembali ke menu utama? y/t: ");
+                    System.out.println("\nHasil :");
+                    System.out.printf("Mean\t= %.1f%n", mean(data));
+                    System.out.print("Apakah anda ingin kembali ke menu utama? y/t: ");
                     Scanner lagi = new Scanner(System.in);
                     m = lagi.next();
+                    }
+                    else
+                        System.err.println("Silahkan masukkan banyak data lebih dari 0");
                 }
-                for (String n = "T"; n.equals("T")||n.equals("t"); ){
-                    System.out.println("Terimakasih");
-                    System.exit(0);
+                else if (abc==2){
+                    System.out.println("");
+                    System.out.print("Masukkan berapa banyak data: ");
+                    Scanner feb = new Scanner(System.in);
+                    input = feb.nextInt();
+                    if (input > 0){
+                        System.out.println("\nMasukkan isi data berupa angka:");
+                    int[] data =  new int[input];
+                    for (int i = 0; i < input; i++)
+                    {
+                        System.out.print("- Isi Data ke-" +(i + 1)+ " = ");
+                        data[i] = feb.nextInt();
+                    }
+                    System.out.println("\nHasil :");
+                    System.out.printf("Median\t= %.1f%n", median(data));
+                    System.out.print("Apakah anda ingin kembali ke menu utama? y/t: ");
+                    Scanner lagi = new Scanner(System.in);
+                    m = lagi.next();
+                    }
+                    else
+                        System.err.println("Silahkan masukkan banyak data lebih dari 0");
+                }
+                else if (abc==3){
+                    System.out.println("");
+                    System.out.print("Masukkan berapa banyak data: ");
+                    Scanner feb = new Scanner(System.in);
+                    input = feb.nextInt();
+                    if (input > 0){
+                        System.out.println("\nMasukkan isi data berupa angka:");
+                    int[] data =  new int[input];
+                    for (int i = 0; i < input; i++)
+                    {
+                        System.out.print("- Isi Data ke-" +(i + 1)+ " = ");
+                        data[i] = feb.nextInt();
+                    }
+                    System.out.println("\nHasil :");
+                    System.out.printf("Modus\t= %s%n", modus(data, input));
+                    System.out.print("Apakah anda ingin kembali ke menu utama? y/t: ");
+                    Scanner lagi = new Scanner(System.in);
+                    m = lagi.next();
+                    }
+                    else
+                        System.err.println("Silahkan masukkan banyak data lebih dari 0");
+                }
+                else{
+                    System.out.println("Terima kasih telah menggunakan program kami");
+                    System.exit(1);
                 }
             }
-            for (String n = "T"; n.equals("T")||n.equals("t"); ){
-                System.out.println("Terimakasih");
-                System.exit(0);
+            catch (Exception e) {
+                System.out.print("Input Salah!");
+                System.out.print("\nCoba Lagi!");
+                System.exit(1);
             }
-
         }
-        catch (InputMismatchException e) {
-            System.out.print("Input Salah!");
+        for(String m = "T"; m.equals("T")||m.equals("t"); ){
+            System.out.println("Terima Kasih");
+            System.exit(1);
         }
     }
-
 
     static String modus(int[] data, int input)
     {
